13aa6045300fa20fb7cd1df6e01dae1c91c313d8
j0nas <jonas.jensen@msn.com>
2016-01-22 19:32:06 +0100

    separated typewriter effect logic into separate class

---
+++ Assets/scripts/ConversationContentAppender.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Random = System.Random;
+
+namespace Assets.scripts
+{
+    public class TypewriterEffectTextAppender
+    {
+        public bool DoneAppending { get; private set; }
+        private readonly Text _textComponent;
+
+        private const float TextSpeedDelaySeconds = 0.05f;
+        private const float TextSpeedRandomnessVariance = 0.1f;
+        private double _currentVariance = 0;
+        private readonly Random _random = new Random();
+
+        private readonly string _dialogueLine;
+        private int _currentAppendingCharacterPosition;
+        private float _lastCharAppendTimeStamp;
+        
+        public TypewriterEffectTextAppender(Text textComponent, string dialogueLine, float lastCharAppendTimeStamp)
+        {
+            _textComponent = textComponent;
+            _dialogueLine = dialogueLine;
+            _lastCharAppendTimeStamp = lastCharAppendTimeStamp;
+
+            // TODO _line length check if (_lines.Length == _currentLineIndex) return;
+            // TODO loosen time.time dependency?
+            // TODO loosen dependency from Text component?
+            _textComponent.text = "";
+            DoneAppending = false;
+        }
+
+        public void UpdateText()
+        {
+            var enoughTimePassedSinceLastCharAppend = Time.time - _lastCharAppendTimeStamp >
+                                                      (TextSpeedDelaySeconds + _currentVariance);
+            if (DoneAppending || !enoughTimePassedSinceLastCharAppend) return;
+
+            _textComponent.text += _dialogueLine[_currentAppendingCharacterPosition++];
+            _lastCharAppendTimeStamp = Time.time;
+            DoneAppending = _currentAppendingCharacterPosition == _dialogueLine.Length;
+            _currentVariance = _random.NextDouble()*TextSpeedRandomnessVariance;
+        }
+    }
+}
--- Assets/scripts/TypewriterUIText.cs
+++ Assets/scripts/TypewriterUIText.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Assets.scripts;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = System.Random;
@@ -9,16 +8,7 @@ public class TypewriterUIText : MonoBehaviour
     public Text DialogueText;
     public TextAsset ConversationXml;
 
-    private const float TextSpeedDelaySeconds = 0.05f;
-    private const float TextSpeedRandomnessVariance = 0.1f;
-
-    private static readonly Random Random = new Random();
-    private double _currentVariance = Random.NextDouble()*TextSpeedRandomnessVariance;
-
-    private string _currentLine;
-    private float _lastCharAppendTime;
-    private int _charPos;
-    private bool _doneAppending;
+    private TypewriterEffectTextAppender _textAppender;
     private DialogueLine[] _lines;
     private int _currentLineIndex;
 
@@ -32,35 +22,21 @@ public class TypewriterUIText : MonoBehaviour
             return;
         }
 
-        LoadCurrentLine();
-        _lastCharAppendTime = Time.time;
+        Click();
     }
 
     public void Click()
     {
+        var dialogueLine = _lines[_currentLineIndex].Name + ": " + _lines[_currentLineIndex].Text;
+        _textAppender = new TypewriterEffectTextAppender(DialogueText, dialogueLine, Time.time);
         _currentLineIndex++;
-        LoadCurrentLine();
-    }
-
-    private void LoadCurrentLine()
-    {
-        if (_lines.Length == _currentLineIndex) return;
-
-        _currentLine = _lines[_currentLineIndex].Name + ": " + _lines[_currentLineIndex].Text;
-        DialogueText.text = "";
-        _charPos = 0;
-        _doneAppending = false;
     }
 
     void Update()
     {
-        var enoughTimePassedSinceLastCharAppend = Time.time - _lastCharAppendTime >
-                                                  (TextSpeedDelaySeconds + _currentVariance);
-        if (_doneAppending || !enoughTimePassedSinceLastCharAppend) return;
-
-        DialogueText.text += _currentLine[_charPos++];
-        _lastCharAppendTime = Time.time;
-        _doneAppending = _charPos == _currentLine.Length;
-        _currentVariance = Random.NextDouble()*TextSpeedRandomnessVariance;
+        if (!_textAppender.DoneAppending)
+        {
+            _textAppender.UpdateText();
+        }
     }
 }
